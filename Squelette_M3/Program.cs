8175a5ebd02648549128ac6cbc810f97e4ff7f3c
fatval <the.valentinboegli@gmail.com>
2026-06-16 09:01:10 +0200

    Commentaire

--- Squelette_M3/Program.cs
+++ Squelette_M3/Program.cs
@@ -11,9 +11,10 @@ namespace Squelette_M3
 
             Application.SetHighDpiMode(HighDpiMode.SystemAware); //Permet à l'application de s'adapter à la résolution de l'écran et ne pas tenir compte du scaling windows
             ApplicationConfiguration.Initialize();
-            DBManager.ConnectToDB("m3", "root", "");
             Application.Run(new FormMain());
 
+            // Initialisation de la connexion à la base de données MySQL (ici projet scolaire avec DB en local, mais en réalité on aurait gérer le MDP différemment, avec un USER SECRET)
+            DBManager.ConnectToDB("m3", "root", "");
 
         }
     }
